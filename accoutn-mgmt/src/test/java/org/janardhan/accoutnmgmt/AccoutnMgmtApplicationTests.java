901ce9a001d2bc014fc221560f9f67fceb17b6db
Janardhan Maithil <maithiljanardan@gmail.com>
2022-05-29 20:29:46 +0530

    bootstrapped account mgmt microservice

---
+++ accoutn-mgmt/src/test/java/org/janardhan/accoutnmgmt/AccoutnMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.janardhan.accoutnmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccoutnMgmtApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
